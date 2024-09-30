2099b0e6fd9b068b504b0d9f4e411105a6a10a9c
Vaishali0617 <vaishalimandloi0617@gmail.com>
2024-09-30 12:19:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -6,9 +6,13 @@ var isValid = function(s) {
     const stack=[];
     const map={
         '(':')',
-        '[':']';
+        '[':']',
 
     };
+
+    for (let char of s){
+        if(map[char])
+    }
 };
 
 module.exports = { isValid };
